70d9a54ce72b3f81661b14792192993366960c40
PedregosaCH <danielpedchacon@gmail.com>
2026-01-13 14:00:16 +0000

    inicio de la clase empleado

---
+++ Empleado.java
@@ -0,0 +1,11 @@
+public class Empleado extends Persona{
+
+    protected double salario_base;
+    protected int antiguedadAnios;
+
+    public Empleado(String dni, String nombre, int edad){
+        
+    }
+
+
+}
