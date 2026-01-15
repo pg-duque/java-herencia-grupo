5c12b21f8d81ca4b3e11d6d8c3aac4a4c4f778c8
pg-duque <pablogardu21@gmail.com>
2026-01-15 10:25:44 +0000

    Añadidos método get y set para personasACargo

--- clases/Manager.java
+++ clases/Manager.java
@@ -5,4 +5,14 @@ public class Manager extends Persona {
         super (dni, nombre, edad);
         this.personasACargo = personasACargo;
     }
+
+    public int getPersonasACargo() {
+        return personasACargo;
+    }
+
+    public void setPersonasACargo(personasACargo) {
+        this.personasACargo = personasACargo;
+    }
+
+    
 }
