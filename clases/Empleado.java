7346face658631a519fc160fc4facb98619cd299
PedregosaCH <danielpedchacon@gmail.com>
2026-01-20 14:25:17 +0100

    "atropellando viejas, que feliz que voy a ser Atropellando viejas, no me van a detener ATROPELLANDO VIEJAS"

---
+++ clases/Empleado.java
@@ -0,0 +1,37 @@
+public class Empleado extends Persona{
+
+    protected double salario_base;
+    protected int antiguedadAnios;
+
+    public Empleado(String dni, String nombre, int edad, double salario_base, int antiguedadAnios){
+        super(dni, nombre, edad);
+        this.salario_base = salario_base;
+        this.antiguedadAnios = antiguedadAnios;
+    }
+
+    public double getSalario_base(){
+        return salario_base;
+    }
+
+    public void setSalario_base(double salario_base){
+        this.salario_base = salario_base;
+    }
+
+    public int getAntiguedadAnios(){
+        return antiguedadAnios;
+    }
+
+    public void setAntiguedadAnios(int antiguedadAnios){
+        this.antiguedadAnios = antiguedadAnios;
+    }
+
+    public double calcularSalario(){
+        double bono = salario_base * antiguedadAnios * 0.05;
+        return salario_base + bono;
+    }
+
+    @override
+    public String toString(){
+        return super.toString() + ", Salario Base: " + salario_base + ", Antiguedad (años): " + antiguedadAnios;
+    }
+}
