89daa36d885132966d5475eb022e81b22b779890
PedregosaCH <danielpedchacon@gmail.com>
2026-01-22 11:01:51 +0100

    "Añadido lo que solicita el ejercicio, junto con un cambio al metodo calcular salario y otro metodo calcularAntiguedad"

--- src/clases/Empleado.java
+++ src/clases/Empleado.java
@@ -1,16 +1,29 @@
 package clases;
 
-import clases.Persona;
+import java.time.LocalDate;
+import java.util.UUID;
 
 public class Empleado extends Persona {
 
     protected double salario_base;
     protected int antiguedadAnios;
+    protected String idEmpleado;
+    protected String departamento;
+    protected double jornadaHoraria;
+    protected boolean activo;
+    protected LocalDate fecha_contratacion;
 
-    public Empleado(String dni, String nombre, int edad, double salario_base, int antiguedadAnios){
+
+    public Empleado(String dni, String nombre, int edad, double salario_base, int antiguedadAnios, String idEmpleado, String departamento, double jornadaHoraria, boolean activo,  LocalDate fecha_contratacion) {
         super(dni, nombre, edad);
         this.salario_base = salario_base;
         this.antiguedadAnios = antiguedadAnios;
+        this.idEmpleado = idEmpleado;
+        this.departamento = departamento;
+        this.fecha_contratacion = fecha_contratacion;
+        this.jornadaHoraria = jornadaHoraria;
+        this.activo = activo;
+
     }
 
     public double getSalario_base(){
@@ -29,9 +42,54 @@ public class Empleado extends Persona {
         this.antiguedadAnios = antiguedadAnios;
     }
 
+
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(String idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public double getJornadaHoraria() {
+        return jornadaHoraria;
+    }
+
+    public void setJornadaHoraria(double jornadaHoraria) {
+        this.jornadaHoraria = jornadaHoraria;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public LocalDate getFecha_contratacion() {
+        return fecha_contratacion;
+    }
+
+    public void setFecha_contratacion(LocalDate fecha_contratacion) {
+        this.fecha_contratacion = fecha_contratacion;
+    }
+
     public double calcularSalario(){
-        double bono = salario_base * antiguedadAnios * 0.05;
+        double bono = salario_base * 0.03;
         return salario_base + bono;
+    }
+
+    public static void calcularAtiguedad(){
+
     }
 
     @Override
