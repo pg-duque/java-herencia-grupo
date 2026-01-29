49fa1a1618d03ad4dbaafca5a89c078c69e7d148
pg-duque <pablogardu21@gmail.com>
2026-01-29 10:43:35 +0100

    subidos cambios con fichaje, tipo fichaje y gestor fichaje

---
+++ src/clases/Fichaje.java
@@ -0,0 +1,59 @@
+package clases;
+
+import java.time.LocalDate;
+
+public class Fichaje {
+    private int idFichaje;
+    private int idEmpleado;
+    private LocalDate fechaHoraEntrada;
+    private LocalDate fechaHoraSalida;
+    private TipoFichaje tipoFichaje;
+
+    public Fichaje(int idFichaje, int idEmpleado, LocalDate fechaHoraEntrada, LocalDate fechaHoraSalida, TipoFichaje tipoFichaje) {
+        this.idFichaje = idFichaje;
+        this.idEmpleado = idEmpleado;
+        this.fechaHoraEntrada = fechaHoraEntrada;
+        this.fechaHoraSalida = fechaHoraSalida;
+        this.tipoFichaje = tipoFichaje;
+    }
+
+    public int getIdFichaje() {
+        return idFichaje;
+    }
+
+    public void setIdFichaje(int idFichaje) {
+        this.idFichaje = idFichaje;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public LocalDate getFechaHoraEntrada() {
+        return fechaHoraEntrada;
+    }
+
+    public void setFechaHoraEntrada(LocalDate fechaHoraEntrada) {
+        this.fechaHoraEntrada = fechaHoraEntrada;
+    }
+
+    public LocalDate getFechaHoraSalida() {
+        return fechaHoraSalida;
+    }
+
+    public void setFechaHoraSalida(LocalDate fechaHoraSalida) {
+        this.fechaHoraSalida = fechaHoraSalida;
+    }
+
+    public TipoFichaje getTipoFichaje() {
+        return tipoFichaje;
+    }
+
+    public void setTipoFichaje(TipoFichaje tipoFichaje) {
+        this.tipoFichaje = tipoFichaje;
+    }
+}
---
+++ src/clases/TipoFichaje.java
@@ -0,0 +1,8 @@
+package clases;
+
+public enum TipoFichaje {
+    ENTRADA,
+    SALIDA,
+    PAUSA
+
+}
